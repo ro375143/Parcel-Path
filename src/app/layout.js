575347c8ef538b237346246fb5d95de1cc50d0fb
wmujahid <warandpeace97@gmail.com>
2024-04-01 17:36:16 -0400

    fixed the page layout - navbar and footer moved to top and bottom of page

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import './globals.css'
 
 
 export default function RootLayout({ children }) {
--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,7 @@ const HomePage = () => {
         <h1>HomePage or Landing Page</h1>
       </div>
       <main>
-        <p>
-          
+        <p>          
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis fermentum ipsum. Duis laoreet est est, venenatis ullamcorper sem accumsan eu. Donec laoreet erat sapien, et efficitur nulla facilisis in. Maecenas vel augue orci. Mauris eu ex porttitor, venenatis lorem et, efficitur justo. Cras consectetur a lorem vel lobortis. Fusce euismod tortor facilisis, tristique elit vel, commodo risus. In in purus lectus. Suspendisse vitae elit fringilla, laoreet arcu ac, porttitor diam. Integer velit nulla, dapibus at vulputate et, bibendum a odio. Sed consectetur faucibus tortor sit amet rutrum. In vel orci bibendum, hendrerit leo at, aliquam ante. Nam sed metus euismod, dignissim justo at, rutrum libero. Nunc vitae elit purus.
         </p>
       </main>
