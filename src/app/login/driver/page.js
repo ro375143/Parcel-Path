ddd4671f1e145588f8fc137a783130fade4c8918
wmujahid <warandpeace97@gmail.com>
2024-04-07 19:35:59 -0400

    update auth

--- src/app/login/driver/page.js
+++ src/app/login/driver/page.js
@@ -1,112 +1,132 @@
-'use client'
-import { signIn } from 'next-auth/react';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../../firebase/firebase';
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/app/firebase/config";
 
-export default function Signin() {
+export default function DriverSignin() {
   const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const signInWithEmailAndPasswordAndRedirect = (email, password) => {
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setErrorMessage("Please enter both email and password.");
+      return;
+    }
+
     try {
-      signInWithEmailAndPassword(auth, email, password) && signIn('credentials', { email, password, redirect: true, callbackUrl: '/' }); //redirect to driver-dashboard
-      
-    } catch (error) {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const uid = userCredential.user.uid.slice(0, 5);
 
-      console.error('Error signing in:', error.message);
-      throw error;
+      router.push(`/driver/${uid}/dashboard`);
+    } catch (error) {
+      console.error("Error signing in:", error);
+      switch (error.code) {
+        case "auth/user-not-found":
+          setErrorMessage("No user found with this email. Please sign up.");
+          break;
+        case "auth/wrong-password":
+          setErrorMessage("Incorrect password. Please try again.");
+          break;
+        default:
+          setErrorMessage("Failed to sign in. Please try again.");
+          break;
+      }
     }
   };
 
   return (
-    <>
-      <div className="flex justify-center items-center h-screen">
-       <div className="max-w-xl bg-white shadow-lg rounded-lg overflow-hidden flex" style={{backgroundColor: '#5b9c7a'}}>
-        <div className="w-56 flex-shrink-auto rounded-lg">
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="max-w-xl bg-white shadow-lg rounded-lg overflow-hidden flex">
+        <div className="w-56 flex-shrink-0 rounded-lg overflow-hidden">
           <img
             className="w-full h-full object-cover"
             src="https://blog.ecabrella.com/hs-fs/hubfs/AdobeStock_299668592.jpeg?width=5569&name=AdobeStock_299668592.jpeg"
-            alt="Your Image"
+            alt="Driver Image"
           />
-          </div>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <div className="flex justify-center">
-              <div style={{ width: '300px', height: '90px', overflow: 'hidden' }}>
-                <img
-                  style={{ width: '100%', height: '120%', objectFit: 'cover'}}
-                  src="https://media.discordapp.net/attachments/1197323344937234464/1225573887639814289/parcel_path_7.png?ex=66219fa0&is=660f2aa0&hm=45169aea1948b7dc2d1feb88e3fd3bd659c090f1d51ff8b11afe9521f84fe9f7&=&format=webp&quality=lossless"
-                  alt="Your Company"
-                />
-              </div>
-            </div>
-          <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-white">
-            Sign in to your account
-          </h2>
         </div>
+        <div className="flex flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-[#5b9c7a]">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <img
+              className="mx-auto h-10 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Company Logo"
+            />
+            <h2 className="mt-6 text-center text-2xl font-extrabold text-white">
+              Driver Sign in to your account
+            </h2>
+          </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <div className="space-y-6">
+          <form onSubmit={handleSignIn} className="mt-8 space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
                 Email address
               </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                />
-              </div>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
 
             <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <div onClick={() => router.push('/forgot-password')} className="cursor-pointer font-bold text-indigo-400 hover:text-indigo-300" style={{color: "#345454"}}>
-                    Forgot password?
-                  </div>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                onClick={() => signInWithEmailAndPasswordAndRedirect(email, password)} //redirect to admin-dashboard
-                disabled={!email || !password}
-                className="disabled:opacity-40 flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2" style={{backgroundColor: '#345454', transition: 'background-color 0.3s' }}
-                onMouseOver={(e) => e.target.style.backgroundColor = '#a4d7bb'}
-                onMouseOut={(e) => e.target.style.backgroundColor = '#345454'}
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-white"
               >
-                Sign in
-              </button>
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-          </div>
+
+            {errorMessage && (
+              <p className="text-red-500 text-center">{errorMessage}</p>
+            )}
+
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+          </form>
+
+          <p className="mt-2 text-center text-sm text-white">
+            Forgot password?{" "}
+            <a
+              onClick={() => router.push("/forgot-password")}
+              className="cursor-pointer font-bold text-indigo-500 hover:text-indigo-700"
+            >
+              Click here
+            </a>
+          </p>
         </div>
       </div>
-      </div>
-      </div>
-    </>
+    </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,48 +1,68 @@
-'use client'
-import React, { useState, useEffect } from 'react';
-import './page.module.css';
-import { redirect } from 'next/navigation';
-import { signOut, useSession } from 'next-auth/react';
-import { auth } from '../firebase/firebase';
-import 'firebase/auth';
+"use client";
+import React, { useState, useEffect } from "react";
+import "./page.module.css";
+import { useRouter } from "next/navigation";
+import { onAuthStateChanged, signOut as firebaseSignOut } from "firebase/auth";
+import { auth } from "@/app/firebase/config";
 
-
-export default function  HomePage() {
-  const session = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect('/login');
-    },
-  })
-
-  const [uid, setUid] = useState(null);
+export default function HomePage() {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setUid(user.uid);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
       } else {
-        setUid(null);
+        setUser(null);
+        router.push("/login");
       }
     });
 
     return () => unsubscribe();
-  }, []); 
+  }, [router]);
+
+  const handleSignOut = () => {
+    firebaseSignOut(auth)
+      .then(() => {
+        router.push("/login");
+      })
+      .catch((error) => {
+        console.error("Sign Out Error", error);
+      });
+  };
 
   return (
     <>
-      <div className='homepage-background'>
-        <div className='text-black'>Email: {session?.data?.user?.email}</div>
-        <div className='text-black'>UID: {uid}</div>
+      <div className="homepage-background">
+        {user ? (
+          <>
+            <div className="text-black">Email: {user.email}</div>
+            <div className="text-black">Email: {user.uid}</div>
+            <button className="text-black" onClick={handleSignOut}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <p>Loading user information...</p>
+        )}
         <h1>HomePage or Landing Page</h1>
       </div>
       <main>
-        <p>          
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis fermentum ipsum. Duis laoreet est est, venenatis ullamcorper sem accumsan eu. Donec laoreet erat sapien, et efficitur nulla facilisis in. Maecenas vel augue orci. Mauris eu ex porttitor, venenatis lorem et, efficitur justo. Cras consectetur a lorem vel lobortis. Fusce euismod tortor facilisis, tristique elit vel, commodo risus. In in purus lectus. Suspendisse vitae elit fringilla, laoreet arcu ac, porttitor diam. Integer velit nulla, dapibus at vulputate et, bibendum a odio. Sed consectetur faucibus tortor sit amet rutrum. In vel orci bibendum, hendrerit leo at, aliquam ante. Nam sed metus euismod, dignissim justo at, rutrum libero. Nunc vitae elit purus.
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          venenatis fermentum ipsum. Duis laoreet est est, venenatis ullamcorper
+          sem accumsan eu. Donec laoreet erat sapien, et efficitur nulla
+          facilisis in. Maecenas vel augue orci. Mauris eu ex porttitor,
+          venenatis lorem et, efficitur justo. Cras consectetur a lorem vel
+          lobortis. Fusce euismod tortor facilisis, tristique elit vel, commodo
+          risus. In in purus lectus. Suspendisse vitae elit fringilla, laoreet
+          arcu ac, porttitor diam. Integer velit nulla, dapibus at vulputate et,
+          bibendum a odio. Sed consectetur faucibus tortor sit amet rutrum. In
+          vel orci bibendum, hendrerit leo at, aliquam ante. Nam sed metus
+          euismod, dignissim justo at, rutrum libero. Nunc vitae elit purus.
         </p>
       </main>
     </>
   );
 }
-
-HomePage.requireAuth = true;
