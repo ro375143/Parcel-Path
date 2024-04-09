aa3e1efb5f9d6e2cb513ac5dcf7a217daa5f5f46
Dhanapati Neopaney <dhanapatineopaney@Dhanapatis-MacBook-Pro.local>
2024-04-09 11:06:11 -0400

    Updated profile page,Where user can edit thier inforamtion.

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -3,37 +3,54 @@ import React, { useContext, useEffect, useState } from "react";
 import { Form, Button, Input } from "antd";
 import { auth, db } from "@/app/firebase/config";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { doc, getDoc, updateDoc } from "@/app/firebase/config";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 
 const UserProfile = (props) => {
   const [form] = Form.useForm();
   const [inputEnabled, setInput] = useState(true);
   const [userInfo, setUserInfo] = useState(null);
 
-  const defaultUser = {
-    userName: "testing@testing.com",
-    password: "123456",
-  };
+  // const defaultUser = {
+  //   userName: "test_customer@test.com",
+  //   password: "test123",
+  // };
+
+  const collectionsName = ["admins", "drivers", "customers"]
+
+  async function getData (cName) {
+    const uRef = doc(db, cName, auth.currentUser.uid);
+    const dSnap = await getDoc(uRef)
+    if (dSnap.data()) {
+      return dSnap.data();
+    }
+    return null;
+  }
 
   const userDatafromDB = async () => {
-    console.log("UID is :" + auth.currentUser.uid);
-    const userRef = doc(db, "user", auth.currentUser.uid);
-    console.log("User Reference is : " + userRef);
-    const documentSnapshot = await getDoc(userRef);
-    console.log(
-      "documentSnapshot is : " +
-        JSON.stringify(documentSnapshot._document.data.value.mapValue.fields)
-    );
-    setUserInfo({
-      firstName:
-        documentSnapshot._document.data.value.mapValue.fields.firstName
-          .stringValue,
-      lastName:
-        documentSnapshot._document.data.value.mapValue.fields.lastName
-          .stringValue,
-      address:
-        documentSnapshot._document.data.value.mapValue.fields.email.stringValue,
-    });
+    let data = null;
+    for (let i = 0; i < collectionsName.length; i++) {
+      data = await getData(collectionsName[i]);
+      if (data != null) {
+        console.log("Data is " + JSON.stringify(data));
+        setUserInfo({
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          email: auth.currentUser.email,
+          type: collectionsName[i],
+          phone: data.phoneNo
+        });
+        return;
+      }
+    }
+    // console.log("UID is :" + auth.currentUser.uid);
+    // const userRef = doc(db, "ff", auth.currentUser.uid);
+    // console.log("User Reference is : " + userRef);
+    // const documentSnapshot = await getDoc(userRef);
+    // console.log(
+    //   "documentSnapshot is : " +
+    //     JSON.stringify(documentSnapshot.data())
+    // );
 
     // setUserInfo({
     //   firstName: "Dhana",
@@ -42,27 +59,27 @@ const UserProfile = (props) => {
     // })
   };
 
-  const signInWithEmailAndPasswordFirebase = async (email, password) => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      // router.route('/admin/id/profile')
-      // setUser(auth.currentUser)
-      userDatafromDB();
-    } catch (error) {
-      console.error("Error signing in:", error.message);
-      throw error;
-    }
-  };
+  // const signInWithEmailAndPasswordFirebase = async (email, password) => {
+  //   try {
+  //     await signInWithEmailAndPassword(auth, email, password);
+  //     // router.route('/admin/id/profile')
+  //     // setUser(auth.currentUser)
+  //     userDatafromDB();
+  //   } catch (error) {
+  //     console.error("Error signing in:", error.message);
+  //     throw error;
+  //   }
+  // };
 
-  const loadDefaultUser = async () => {
-    signInWithEmailAndPasswordFirebase(
-      defaultUser.userName,
-      defaultUser.password
-    );
-    console.log(
-      "CURRENT USER FROM DEFAULT is: " + JSON.stringify(auth.currentUser)
-    );
-  };
+  // const loadDefaultUser = async () => {
+  //   signInWithEmailAndPasswordFirebase(
+  //     defaultUser.userName,
+  //     defaultUser.password
+  //   );
+  //   console.log(
+  //     "CURRENT USER FROM DEFAULT is: " + JSON.stringify(auth.currentUser)
+  //   );
+  // };
 
   useEffect(() => {
     if (userInfo) {
@@ -70,6 +87,9 @@ const UserProfile = (props) => {
         firstName: userInfo.firstName,
         lastName: userInfo.lastName,
         address: userInfo.address,
+        email: userInfo.email,
+        type: userInfo.type,
+        phone: userInfo.phone
       });
     }
   });
@@ -83,11 +103,11 @@ const UserProfile = (props) => {
     setInput(true);
     console.log("submit user clicked " + inputEnabled);
 
-    //TODO make sure that the doc gets updated
-    await updateDoc(doc(db, "user", auth.currentUser.uid), {
+    await updateDoc(doc(db, userInfo.type, auth.currentUser.uid), {
       firstName: userInfo.firstName,
       lastName: userInfo.lastName,
       address: userInfo.address,
+      phoneNo: userInfo.phone
     });
   };
 
@@ -104,15 +124,28 @@ const UserProfile = (props) => {
     setUserInfo({ ...userInfo, address: ev.target.value });
   }
 
+  function handlePhoneChange(ev) {
+    setUserInfo({ ...userInfo, phone: ev.target.value });
+  }
+
   const cancelEditMode = () => {
     setInput(true);
     console.log("submit user clicked " + inputEnabled);
   };
 
   if (!userInfo) {
-    loadDefaultUser();
-    console.log("THERE IS NO USER");
+    if (!auth.currentUser) {
+      console.log("THERE IS NO USER");
+      //loadDefaultUser();
+    } else {
+      console.log("There is a user.. Loading the info now")
+      userDatafromDB();
+    }
   }
+  // if (!userInfo) {
+  //   loadDefaultUser();
+  //   console.log("THERE IS NO USER");
+  // }
 
   return (
     <div>
@@ -151,11 +184,32 @@ const UserProfile = (props) => {
           <Input disabled={inputEnabled} onChange={handleLastNameChange} />
         </Form.Item>
         <Form.Item
+          name="email"
+          label="Email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input disabled={true} />
+        </Form.Item>
+        <Form.Item
           name="address"
           label="Address"
           rules={[{ required: true, message: "Please input your address!" }]}
         >
           <Input disabled={inputEnabled} onChange={handleAddressChange} />
+        </Form.Item>
+        <Form.Item
+          name="type"
+          label="Type"
+          rules={[{ required: true, message: "Type of user!" }]}
+        >
+          <Input disabled={true} />
+        </Form.Item>
+        <Form.Item
+          name="phone"
+          label="Phone Number"
+          rules={[{ required: true, message: "Phone Number!" }]}
+        >
+          <Input disabled={inputEnabled} onChange={handlePhoneChange}/>
         </Form.Item>
         {inputEnabled ? (
           <Button onClick={editUser} disabled={!inputEnabled}>
