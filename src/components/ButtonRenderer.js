4e00ab6660abcf7da72dd8c862b1f355319d32f0
Esoteric <irritas8485@gmail.com>
2024-04-09 20:39:19 -0400

    improved ui

--- src/components/ButtonRenderer.js
+++ src/components/ButtonRenderer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-
+import './grids.css';
 const ButtonRenderer = ({ params, onEdit, onDelete }) => {
   return (
     <div style={{ display: 'flex', gap: '8px' }}>
       <Button
+        className='action-button'
         type="primary"
         icon={<EditOutlined />}
         onClick={() => onEdit(params.data.id)}
@@ -13,6 +14,7 @@ const ButtonRenderer = ({ params, onEdit, onDelete }) => {
         Edit
       </Button>
       <Button
+      className='action-button'
         danger
         icon={<DeleteOutlined />}
         onClick={() => onDelete(params.data.id)}
