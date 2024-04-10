b391381ce14f13c2cbb113edeaeba8751be1d589
wmujahid <warandpeace97@gmail.com>
2024-04-09 23:08:47 -0400

    avity-admin-branch

--- src/components/PackageGrid.js
+++ src/components/PackageGrid.js
@@ -18,12 +18,19 @@ import { Button, Input, Row, Col, Modal } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import dayjs from "dayjs";
 import { Timestamp } from "firebase/firestore";
+import './grids.css';
+import { saveAs } from 'file-saver';
+import { json2csv } from 'json-2-csv';
+import moment from "moment";
 
 const PackagesGrid = () => {
   const [rowData, setRowData] = useState([]);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [currentPackage, setCurrentPackage] = useState(null);
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false); // New state for create modal visibility
+  const [searchValue, setSearchValue] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  
 
   const columns = [
     { headerName: "ID", field: "id", flex: 1 },
@@ -150,15 +157,48 @@ const PackagesGrid = () => {
     setRowData(packagesArray);
   };
 
+  const handleSearch = (value) => {
+    setSearchValue(value);
+    if (value) {
+      const filtered = rowData.filter((row) => {
+        return Object.keys(row).some((field) => {
+          return row[field].toString().toLowerCase().includes(value.toLowerCase());
+        });
+      });
+      setFilteredData(filtered);
+    } else {
+      setFilteredData(rowData);
+    }
+  }
+
+  const exportData  = async () => {
+    const querySnapshot = await getDocs(collection(db, "packages"));
+    const packagesArray = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    const csvData = packagesArray.map((item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        status: item.status,
+        customerId: item.customerId,
+        trackingNumber: item.trackingNumber,
+        packageWeight: item.packageWeight,
+        packageDimensions: item.packageDimensions,
+        shipDate: item.shipDate?.toDate ? moment(item.shipDate.toDate()).format("MM-DD-YYYY") : '',
+        deliveryDate: item.deliveryDate?.toDate ? moment(item.deliveryDate.toDate()).format("MM-DD-YYYY") : '',
+      };
+    }
+    );
+    const csv = await json2csv(csvData);
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8' });
+    saveAs(blob, 'packages.csv');
+  }
+
   return (
     <div>
       <Row gutter={16} style={{ marginBottom: 16 }}>
         <Col>
-          <Input.Search
-            placeholder="Search packages..."
-            onSearch={(value) => console.log(value)}
-            enterButton
-          />
+        <Input.Search className='search-input' placeholder="Search packages..." onSearch={handleSearch} value={searchValue} onChange={(e) => handleSearch(e.target.value)} />
         </Col>
         <Col>
           <Button
@@ -169,12 +209,20 @@ const PackagesGrid = () => {
             Add Package
           </Button>
         </Col>
+        <Col>
+          <Button className='action-button' type="primary" onClick={exportData} style={{margin: '20px', justifyContent: 'left', display: 'flex'}}
+          >
+            Export Data
+          </Button>
+        </Col>
       </Row>
       <div className="ag-theme-alpine" style={{ width: "100%" }}>
         <AgGridReact
-          rowData={rowData}
+          rowData={filteredData}
           columnDefs={columns}
           domLayout="autoHeight"
+          rowHeight={40}
+          style={{ borderRadius: '10px', overflow: 'hidden' }}
         />
       </div>
       {isEditModalOpen && currentPackage && (
