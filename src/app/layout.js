b2385d5dbce4baa141cb4dd4a57d071ac2b0d7c9
emmalwalker <emmawalker145@yahoo.com>
2024-04-15 12:32:45 -0400

    New one

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,19 @@ import Footer from "@/components/Footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full bg-gray-900">
-      <body style={{ backgroundColor: "#f4e8c4", minHeight: "120vh" }}>
-        <Navbar />
-        <div className="flex-1">{children}</div>
-        <Footer />
-      </body>
-    </html>
+      <body 
+      style={{
+        backgroundImage: "https://cdn.discordapp.com/attachments/1197323344937234464/1229457882588647536/bgphoto.png?ex=662fc0e1&is=661d4be1&hm=19525f4cebbe33f68bb5595e95905c365a9be74214146080168a8b1a616971c6&",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundColor: "#f4e8c4",
+        minHeight: "120vh",
+      }}
+    >
+      <Navbar />
+      <div className="flex-1">{children}</div>
+      <Footer />
+    </body>
+  </html>
   );
 }
