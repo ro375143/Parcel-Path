48875d63a5130982470506bb6016e8bbf1d0e9ca
emmalwalker <emmawalker145@yahoo.com>
2024-04-15 11:44:06 -0400

    New commit. Can't figure out why bg image won't show.

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,12 @@ export default function HomePage() {
     <div className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.slogan}>Parcel Path, a business tool for the future.</h1>
+        <div className={styles.pitch}>
+          <p className={styles.pitchText}>Bringing ease and accessibility to monitoring your packages.</p>
+        </div>
+        <div className={styles.line}>
+          <p className={styles.lineText}>Check out our comprehensive suite of delivery solutions designed to streamline the shipping process, all from the comfort of your home.</p>
+        </div>
         <button onClick={() => router.push("/tracking")} className={styles.trackPackageButton}>TRACK YOUR PACKAGES FOR FREE, FOREVER!</button> {/* Corrected className */}
         <div className={styles.businessInfo}>
           <p>Are you a small business? We support you! Register your business for FREE today!</p>
